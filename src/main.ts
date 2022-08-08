53a9fc30f14a6f4c25bf2569824af05fcec5f975
VolodymyrBohutskyiGM <89253905+VolodymyrBohutskyiGM@users.noreply.github.com>
2022-08-08 19:18:45 +0300

    fs 05

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 import * as core from '@actions/core';
-import * as fs from "fs"; 
+import { context, getOctokit } from "@actions/github";
+import * as dedent from 'dedent';
 
-async function main() {
-  const path = './'
-  const fileName = core.getInput('Name');
-  fs.writeFile(path + fileName + '.txt', 'Hello content!', function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  })
+const ghToken = core.getInput("ghToken");
+
+getDiff().then(files => {
+  console.log(dedent(`
+  Your PR diff:
+  ${JSON.stringify(files, undefined, 2)}
+  `))
+})
+
+async function getDiff() {
+  if (ghToken && context.payload.pull_request) {
+      const octokit = getOctokit(ghToken)
+
+      const result = await octokit.rest.repos.compareCommits({
+          repo: context.repo.repo,
+          owner: context.repo.owner,
+          head: context.payload.pull_request.head.sha,
+          base: context.payload.pull_request.base.sha,
+          per_page: 100
+      })
+
+      return result.data.files || []
+  }
+
+  return []
 }
-
-main();
