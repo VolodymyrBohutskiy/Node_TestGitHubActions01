00bbdfc13092f7468821697976499bca08ef51c5
VolodymyrBohutskyiGM <89253905+VolodymyrBohutskyiGM@users.noreply.github.com>
2022-08-08 15:58:09 +0300

    fs 02

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import * as core from '@actions/core';
 import * as fs from "fs"; 
 
 async function main() {
-  const path = './../newfiles/'
+  const path = './'
   const fileName = core.getInput('Name');
-  fs.writeFile(path + fileName, 'Hello content!', function (err) {
+  fs.writeFile(path + fileName + '.txt', 'Hello content!', function (err) {
     if (err) throw err;
     console.log('Saved!');
   })
